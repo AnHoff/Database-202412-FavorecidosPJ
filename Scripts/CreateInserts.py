b47b0389abbd17f5e3f5eb43ba0a6fea3f2bb5d4
AnHoff <ana.luiza.hoff@gmail.com>
2025-02-17 15:09:23 -0300

    Corrige bug script de criação

--- Scripts/CreateInserts.py
+++ Scripts/CreateInserts.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import unicodedata
 
 file_paths = {
     'CNAE': r'202412_CNAE.csv',                           # Substitua pelo caminho real do arquivo CSV CNAE
@@ -6,25 +7,28 @@ file_paths = {
     'CNPJ': r'202412_CNPJ.csv'                            # Substitua pelo caminho real do arquivo CSV CNPJ
 }
 
-# Remover apóstrofos
 def remove_apostrophes(value):
     if isinstance(value, str):
         return value.replace("'", "")
     return value
 
-# Verificar se há valores 'nan' ou NaN
+def remove_diacritics(value):
+    if isinstance(value, str):
+        return ''.join(
+            c for c in unicodedata.normalize('NFKD', value) if not unicodedata.combining(c)
+        )
+    return value
+
 def contains_nan(row):
     return row.isnull().any() or (row.astype(str) == 'nan').any() or (row.astype(str) == '0').any()
 
 sql_inserts = []
 
-# Adicionando a linha para desabilitar a verificação de chaves estrangeiras no início do script
 sql_inserts.append("-- Desabilitar a verificação de chaves estrangeiras")
 sql_inserts.append("SET foreign_key_checks = 0;")
 
 for table_name, file_path in file_paths.items():
     try:
-        # Ler o arquivo CSV
         data = pd.read_csv(file_path, encoding='latin1', delimiter=';', quotechar='"')
 
         print(f"{table_name}: {len(data)} linhas lidas.")
@@ -34,10 +38,8 @@ for table_name, file_path in file_paths.items():
             continue
 
         for _, row in data.iterrows():
-            # Remover apóstrofos de todos os valores da linha
-            row = row.apply(remove_apostrophes)
+            row = row.apply(remove_apostrophes).apply(remove_diacritics)
 
-            # Verificar se a linha contém NaN (valores ausentes) ou 'nan'
             if contains_nan(row):
                 continue  # Pula essa linha se ela contiver NaN ou 'nan'
 
@@ -49,8 +51,7 @@ for table_name, file_path in file_paths.items():
     except Exception as e:
         print(f"Erro ao ler o arquivo {file_path}: {e}")
 
-# Salvar os comandos SQL em um arquivo
-with open('Scripts/popula.sql', 'w', encoding='utf-8') as file:
+with open('Scripts/populaFiltrado.sql', 'w', encoding='utf-8') as file:
     file.write("\n".join(sql_inserts))
 
 print("Arquivo SQL gerado com sucesso!")
--- Scripts/scriptFisico.sql
+++ Scripts/scriptFisico.sql
@@ -4,25 +4,25 @@ USE 202412_FavorecidosPJ;
 CREATE TABLE IF NOT EXISTS CNAE (
     COD_SECAO CHAR(1) NOT NULL,              			-- Código da seção (letra única)
     DESC_SECAO VARCHAR(255) NOT NULL,        		
-    COD_SUBCLASSE INT(9) NOT NULL,              		-- Código da subclasse
+    COD_SUBCLASSE INT NOT NULL,              		-- Código da subclasse
     DESC_SUBCLASSE VARCHAR(255) NOT NULL,     		
     PRIMARY KEY (COD_SUBCLASSE)
 );
 
 CREATE TABLE IF NOT EXISTS NaturezaJuridica (
-    COD_NATJURIDICA INT(4) NOT NULL,               		-- Código da natureza jurídica
+    COD_NATJURIDICA INT NOT NULL,               		-- Código da natureza jurídica
     DESC_NATJURIDICA VARCHAR(255) NOT NULL,    		
-    COD_TIPO_NATJURIDICA INT(9) NOT NULL,         		-- Código do tipo de natureza jurídica
+    COD_TIPO_NATJURIDICA INT NOT NULL,         		-- Código do tipo de natureza jurídica
     DESC_TIPO_NATJURIDICA VARCHAR(255) NOT NULL, 	
 	PRIMARY KEY (COD_NATJURIDICA)
 );
 
 CREATE TABLE IF NOT EXISTS CNPJ (
-    CNPJ bigint NOT NULL,                          
+    CNPJ BIGINT NOT NULL,                          
     RAZAOSOCIAL VARCHAR(255) NOT NULL,          
     NOMEFANTASIA VARCHAR(255),                  
-    COD_CNAE INT(9) NOT NULL,                           -- Código CNAE
-    COD_NATJURIDICA INT(4) NOT NULL,                    -- Código da natureza jurídica
+    COD_CNAE INT,                           			-- Código CNAE
+    COD_NATJURIDICA INT,                   			-- Código da natureza jurídica
     TIPO_PESSOA VARCHAR(255),                   
     LOGRADOURO VARCHAR(255),                    
     NUMERO VARCHAR(50),                         		-- Número (aceita 'SN' ou outros textos)
@@ -41,20 +41,3 @@ CREATE TABLE IF NOT EXISTS CNPJ (
         ON DELETE SET NULL
         ON UPDATE CASCADE
 );
-
--- Usuário administrador
-CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';
-GRANT ALL PRIVILEGES ON 202412_favorecidospj.* TO 'admin'@'%';  -- Privilegio excessivo em todos os bancos
-FLUSH PRIVILEGES;
-
--- Usuário com privilégios excessivos
-CREATE USER 'consultor'@'%' IDENTIFIED BY 'consultor';
-GRANT ALL PRIVILEGES ON 202412_favorecidospj.* TO 'consultor'@'%';
-GRANT GRANT OPTION ON 202412_favorecidospj.* TO 'consultor'@'%';
-FLUSH PRIVILEGES;
-
--- Usuário com permissões para alterar dados em qualquer banco de dados
-CREATE USER 'advogado'@'%' IDENTIFIED BY 'advogado';
-GRANT INSERT, UPDATE, DELETE ON 202412_favorecidospj.* TO 'advogado'@'%';  -- Privilégios excessivos de alteração de dados
-FLUSH PRIVILEGES;
-
